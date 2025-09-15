19273ba3ee8aca1d5bbce34c68ad73715ee5d9bd
RaymondSWE <RamanMohammed@hotmail.com>
2025-09-15 15:09:14 +0200

    refactor: add guard clause for existing id to deleteArticle in the service implication

--- server/src/main/java/org/vgr/server/service/impl/ArticleServiceImpl.java
+++ server/src/main/java/org/vgr/server/service/impl/ArticleServiceImpl.java
@@ -33,8 +33,8 @@ public class ArticleServiceImpl implements ArticleService {
 
     @Override
     public void deleteArticle(Long id) {
-        articleRepository.deleteById(id);
-
+        if(!articleRepository.existsById(id)) throw new ArticleNotFoundException(id);
+         articleRepository.deleteById(id);
     }
 
     @Override
